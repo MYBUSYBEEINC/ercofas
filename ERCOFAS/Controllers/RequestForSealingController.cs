3e32f979658d04e072094293c5e90de8d0ee51f2
Geralmybusybee <gerald@mybusybee.net>
2023-02-23 17:57:36 +0800

    Add the new field for review and create route for request for sealing and validation meter sampling

---
+++ ERCOFAS/Controllers/RequestForSealingController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using ERCOFAS.Models;
+using ERCOFAS.Resources;
+
+namespace ERCOFAS.Controllers
+{
+    [Authorize]
+    public class RequestForSealingController : Controller
+    {
+        private DefaultDBContext db = new DefaultDBContext();
+        private GeneralController general = new GeneralController();
+
+        /// <summary>
+        /// [CustomAuthorizeFilter(ProjectEnum.ModuleCode.CaseType, "true", "", "", "")]
+        /// </summary>
+        /// <returns></returns>
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (db != null)
+                {
+                    db.Dispose();
+                    db = null;
+                }
+                if (general != null)
+                {
+                    general.Dispose();
+                    general = null;
+                }
+            }
+
+            base.Dispose(disposing);
+        }
+    }
+}
