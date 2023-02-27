9cfb4361e8b769e37bb2eb4e61d95bebd394b288
Geraldmybusybee <gerald@mybusybee.net>
2023-02-27 17:53:30 +0800

    Routing for edit / view and apply ui design. Add database connection , models, controllers. Note: Not yet connected to ui.

--- ERCOFAS/Controllers/RequestSealingAcceptanceController.cs
+++ ERCOFAS/Controllers/RequestSealingAcceptanceController.cs
@@ -8,6 +8,8 @@ using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
 using ERCOFAS.Models;
 using ERCOFAS.Resources;
+using ERCOFAS.Enumeration;
+using ERCOFAS.Helpers;
 
 namespace ERCOFAS.Controllers
 {
@@ -21,9 +23,202 @@ namespace ERCOFAS.Controllers
         /// [CustomAuthorizeFilter(ProjectEnum.ModuleCode.CaseType, "true", "", "", "")]
         /// </summary>
         /// <returns></returns>
-        public ViewResult Index()
+        public ActionResult Index()
+        {
+            if (RoleHelpers.GetMainRole() == UserTypeEnum.Client.ToString())
+            {
+                string userId = User.Identity.GetUserId();
+
+                int preFilings = db.SealingAndAcceptanceAttachments.Count(x => x.UserId == userId);
+                if (preFilings == 0)
+                    return Redirect("/RequestSealingAcceptance/NoFiledYet");
+            }
+
+            return View();
+        }
+
+        public ActionResult NoFiledYet()
         {
             return View();
+        }
+
+        public ActionResult GetPartialViewSealingAndAcceptance()
+        {
+            string userId = User.Identity.GetUserId();
+            SealingAndAcceptanceListing theListing = new SealingAndAcceptanceListing();
+            theListing.Listing = ReadSealingAndAcceptance(userId);
+            return PartialView("~/Views/SealingAndAcceptance/_MainList.cshtml", theListing);
+        }
+
+        public List<SealingAndAcceptanceViewModel> ReadSealingAndAcceptance(string userId)
+        {
+            List<SealingAndAcceptanceViewModel> list = new List<SealingAndAcceptanceViewModel>();
+            list = (from t1 in db.SealingAndAcceptances.AsNoTracking()
+                    join t2 in db.AspNetUsers.AsNoTracking() on t1.CreatedBy equals t2.Id
+                    join t3 in db.UserProfiles.AsNoTracking() on t1.Stakeholder equals t3.AspNetUserId
+                    join t8 in db.AspNetRoles.AsNoTracking() on t7.RoleId equals t8.Id
+                    orderby t1.CreatedOn descending
+                    select new PreFiledCaseViewModel
+                    {
+                        Id = t1.Id,
+                        RequestDescription = t1.RequestDescription,
+                        Date = t1.Date,
+                        Time = t1.Time,
+                        Details = t1.Details,
+                        Remarks = t1.Remarks,
+                        NotifyBilling = t1.NotifyBilling,
+                        ReviewStatus = t1.ReviewStatus,
+                        PaymentStatus = t1.PaymentStatus,
+                        AssignedNumber = t1.AssignedNumber,
+                        AssignedPersonel = t1.AssignedPersonel,
+                        AssignedPersonnelStatus = t1.AssignedPersonnelStatus,
+                        Supervisor = t1.Supervisor,
+                        SupervisorStatus = t1.SurvivorStatus,
+                        DivisionChief = t1.DivisionChief,
+                        DivisionChiefStatus = t1.DivisionChiefStatus,
+                        Director = t1.Director,
+                        DirectorAssignedNumber = t1.DirectorAssignedNumber,
+                        OED = t1.OED,
+                        TravelAuthorityApprovalStatus = t1.TravelAuthorityApprovalStatus,
+                    }).OrderByDescending(x => x.CreatedOn).ToList();
+
+            var userRole = RoleHelpers.GetMainRole();
+            if (userRole != null && userRole == UserTypeEnum.Client.ToString())
+                list = list.Where(x => x.UserId == userId).ToList();
+
+            return list;
+        }
+
+        public List<SealingAndAcceptanceAttachment> ReadSealingAndAcceptance(string sealingAcceptanceId)
+        {
+            List<SealingAndAcceptanceAttachment> list = new List<SealingAndAcceptanceAttachment>();
+            list = db.SealingAndAcceptanceAttachments.Where(x => x.SealingAndAcceptanceId == sealingAcceptanceId).ToList();
+            return list;
+        }
+
+
+        public SealingAndAcceptanceViewModel GetViewModel(string Id, string type)
+        {
+            SealingAndAcceptanceViewModel model = new SealingAndAcceptanceViewModel();
+            using (DefaultDBContext db = new DefaultDBContext())
+            {
+                SealingAndAcceptance sealingAcceptance = db.SealingAndAcceptances.Where(a => a.Id == Id).FirstOrDefault();
+                AspNetUserRoles role = db.AspNetUserRoles.Where(a => a.UserId == sealingAcceptance.UserId).FirstOrDefault();
+                model.Id = sealingAcceptance.Id;
+                model.DocumentRequest = sealingAcceptance.DocumentRequest;
+                model.Stakeholder = db.UserProfiles.FirstOrDefault(x => x.AspNetUserId == sealingAcceptance.Stakeholder).FullName;
+                model.Remarks = sealingAcceptance.Remarks;
+                model.Date = sealingAcceptance.Date;
+                model.Time = sealingAcceptance.Time;
+                model.ReviewStatus = sealingAcceptance.ReviewStatus;
+                model.NotifyBilling = sealingAcceptance.NotifyBilling;
+                model.PaymentStatus = sealingAcceptance.PaymentStatus;
+                model.AssignedNumber = sealingAcceptance.AssignedNumber;
+                model.AssignedPersonnel = sealingAcceptance.AssignedPersonnel;
+                model.Supervisor = sealingAcceptance.Supervisor;
+                model.SupervisorStatus = sealingAcceptance.SupervisorStatus;
+                model.DivisionChief = sealingAcceptance.DivisionChief;
+                model.DivisionChiefStatus = sealingAcceptance.DivisionChiefStatus;
+                model.Director = sealingAcceptance.Director;
+                model.DirectorAssignedNumber = sealingAcceptance.DirectorAssignedNumber;
+                model.OED = sealingAcceptance.OED;
+                model.TravelAuthorityApprovalStatus = sealingAcceptance.TravelAuthorityApprovalStatus;
+                model.PaymentStatus = sealingAcceptance.PaymentStatus;
+                model.CreatedOn = sealingAcceptance.CreatedOn;
+                model.ModifiedOn = sealingAcceptance.ModifiedOn;
+            }
+            return model;
+        }
+
+       /// <summary>
+       /// [CustomAuthorizeFilter(ProjectEnum.ModuleCode.PreFiledCase, "", "true", "true", "")]
+       /// </summary>
+       /// <param name="Id"></param>
+       /// <returns></returns>
+        public ActionResult DeleteFile(string Id)
+        {
+            if (Id != null)
+            {
+                var attachment = db.SealingAndAcceptanceAttachments.FirstOrDefault(x => x.Id == Id);
+                db.SealingAndAcceptanceAttachments.Remove(attachment);
+                db.SaveChanges();
+
+                if (System.IO.File.Exists(attachment.FileUrl))
+                    System.IO.File.Delete(attachment.FileUrl);
+
+                TempData["NotifySuccess"] = "File was deleted successfully.";
+            }
+            return Redirect(Request.UrlReferrer.ToString());
+        }
+
+        /// <summary>
+        /// [CustomAuthorizeFilter(ProjectEnum.ModuleCode.PreFiledCase, "", "true", "true", "")]
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns></returns>
+        public ActionResult Edit(string Id)
+        {
+            SealingAndAcceptanceViewModel model = new SealingAndAcceptanceViewModel();
+            if (Id != null)
+            {
+                model = GetViewModel(Id, "Edit");
+            }
+            model.Attachments = ReadSealingAndAcceptance(Id);
+
+            return View(model);
+        }
+
+        /// <summary>
+        /// [CustomAuthorizeFilter(ProjectEnum.ModuleCode.PreFiledCase, "true", "", "", "")]
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns></returns>
+        public ActionResult ViewRecord(string Id)
+        {
+            PreFiledCaseViewModel model = new PreFiledCaseViewModel();
+            if (Id != null)
+            {
+                model = GetViewModel(Id, "View");
+            }
+            model.Attachments = ReadPreFiledAttachments(Id);
+            model.PreFiledAttachmentViewModels = GetPreFiledAttachments(Id);
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(SealingAndAcceptanceViewModel model)
+        {
+            ValidateModel(model);
+
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            SaveRecord(model);
+            TempData["NotifySuccess"] = Resource.RecordSavedSuccessfully;
+            return RedirectToAction("index");
+        }
+
+        public void ValidateModel(SealingAndAcceptanceViewModel model)
+        {
+            if (model != null)
+            {
+                bool duplicated = false;
+                if (model.Id != null)
+                {
+                    duplicated = db.SealingAndAcceptances.Where(a => a.RequestDocument == model.RequestDocument && a.Id != model.Id).Any();
+                }
+                else
+                {
+                    duplicated = db.SealingAndAcceptances.Where(a => a.RequestDocument == model.RequestDocument).Select(a => a.Id).Any();
+                }
+
+                if (duplicated == true)
+                {
+                    ModelState.AddModelError("RequestSubject", Resource.RequestSubjectAlreadyExist);
+                }
+            }
         }
 
         protected override void Dispose(bool disposing)
--- ERCOFAS/Models/SealingAndAcceptanceModels.cs
+++ ERCOFAS/Models/SealingAndAcceptanceModels.cs
@@ -42,6 +42,8 @@ namespace ERCOFAS.Models
         [MaxLength(128)]
         public string Id { get; set; }
         public string Stakeholder { get; set; }
+        [DataType(DataType.MultilineText)]
+        [Display(Name = "RequestDescription", ResourceType = typeof(Resource))]
         public string RequestDescription { get; set; }
         public DateTime Date { get; set; }
         public DateTime Time { get; set; }
@@ -62,8 +64,21 @@ namespace ERCOFAS.Models
         public string OED { get; set; }
         public string TravelAuthorityStatus { get; set; }
         public string ModifiedBy { get; set; }
-        public DateTime ModifiedOn { get; set; }
+        public DateTime? ModifiedOn { get; set; }
         public string CreatedBy { get; set; }
-        public DateTime CreatedOn { get; set; }
+        public DateTime? CreatedOn { get; set; }
+        public HttpPostedFileBase[] Files { get; set; }
+        public List<SealingAndAcceptanceAttachment> Attachments { get; set; }
+        public IList<PreFiledAttachmentViewModel> PreFiledAttachmentViewModels { get; set; }
+        public List<HttpPostedFileBase> DocumentRequest { get; set; }
+        public List<HttpPostedFileBase> StatementOfAccount { get; set; }
+        public List<HttpPostedFileBase> TravelAuthority { get; set; }
+        public List<HttpPostedFileBase> AttachSignature { get; set; }
+        public List<HttpPostedFileBase> ProofOfPayment { get; set; }
+    }
+
+    public class SealingAndAcceptanceListing
+    {
+        public List<SealingAndAcceptanceViewModel> Listing { get; set; }
     }
 }
